53cf4b6ca7a8a61d357062dee8b5c38a95efaac4
Robin <irowbin@gmail.com>
2020-04-28 11:13:13 +0545

    add more fake data.

--- src/storage/database.js
+++ src/storage/database.js
@@ -7,7 +7,7 @@ export const employees = [
         gender: 'male',
         email: 'john@email.com',
         account: {
-            username: 'john',
+            username: 'john123',
             password: '1234',
             role: 'manager'
         },
@@ -56,7 +56,7 @@ export const employees = [
         gender: 'female',
         email: 'jane@email.com',
         account: {
-            username: 'john',
+            username: 'jane99',
             password: '1234',
             role: 'manager'
         },
@@ -96,5 +96,495 @@ export const employees = [
                 }
             ]
         }
-    }
+    },
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'doe',
+        age: 30,
+        gender: 'male',
+        email: 'john@email.com',
+        account: {
+            username: 'john123',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'doe',
+        age: 30,
+        gender: 'female',
+        email: 'jane@email.com',
+        account: {
+            username: 'jane99',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'doe',
+        age: 30,
+        gender: 'male',
+        email: 'john@email.com',
+        account: {
+            username: 'john123',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'doe',
+        age: 30,
+        gender: 'female',
+        email: 'jane@email.com',
+        account: {
+            username: 'jane99',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'doe',
+        age: 30,
+        gender: 'male',
+        email: 'john@email.com',
+        account: {
+            username: 'john123',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'doe',
+        age: 30,
+        gender: 'female',
+        email: 'jane@email.com',
+        account: {
+            username: 'jane99',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'doe',
+        age: 30,
+        gender: 'male',
+        email: 'john@email.com',
+        account: {
+            username: 'john123',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'doe',
+        age: 30,
+        gender: 'female',
+        email: 'jane@email.com',
+        account: {
+            username: 'jane99',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'doe',
+        age: 30,
+        gender: 'male',
+        email: 'john@email.com',
+        account: {
+            username: 'john123',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'doe',
+        age: 30,
+        gender: 'female',
+        email: 'jane@email.com',
+        account: {
+            username: 'jane99',
+            password: '1234',
+            role: 'manager'
+        },
+        address: {
+            temporary: {
+                street: ' 5th avenue',
+                zip: 1233,
+                city: 'awesome city',
+                country: 'Japan',
+            },
+            permanent: {
+                street: ' 15th avenue',
+                zip: 4434,
+                city: 'cool city',
+                country: 'United States',
+            },
+        },
+        contact: {
+            phoneNumbers: [
+                {
+                    type: ' Office',
+                    number: 1231239123
+                },
+                {
+                    type: ' Home',
+                    number: 8123812311
+                }
+            ],
+            mailbox: [
+                {
+                    type: ' Office',
+                    email: 'asd@mail.com'
+                },
+                {
+                    type: ' Home',
+                    email: 'cool@mail.com'
+                }
+            ]
+        }
+    },
 ]
