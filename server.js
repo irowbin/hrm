82a00d36bd6044c4426c4322be7d504866b0dfa8
Robin <irowbin@gmail.com>
2020-05-02 14:41:58 +0545

    use express js to serve

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path')
 
 const app = express()
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+server.listen(3000);
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
@@ -15,3 +18,16 @@ app.get(/.*/, function (req, res) {
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
+
+io.on('connection', (socket) => {
+    socket.on('addOrUpdateEmployee', (data) => {
+        console.log('an employee is created: ', data)
+        socket.broadcast.emit('employeeCreatedOrUpdated', data);
+    });
+    socket.on('deleteEmployee', (id) => {
+        console.log('an employee is deleted: ',id)
+        socket.broadcast.emit('employeeDeleted', id);
+    });
+    socket.on('disconnect', () => {
+    });
+});
